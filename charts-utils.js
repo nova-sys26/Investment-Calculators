ec8d4fb43517482a3ac2d55cdec38d15c45d5c25
Nova-sys <tugrasina@gmail.com>
2026-04-09 21:36:45 +0300

    Create charts-utils.js

---
+++ charts-utils.js
@@ -0,0 +1,42 @@
+let chart;
+
+function drawProfitChart(canvasId, investment, value){
+
+let ctx = document.getElementById(canvasId);
+
+if(chart){
+chart.destroy();
+}
+
+let profit = value - investment;
+
+let finalColor;
+
+if(profit >= 0){
+finalColor = '#22c55e'; // profit → green
+}else{
+finalColor = '#ef4444'; // loss → red
+}
+
+chart = new Chart(ctx,{
+type:'bar',
+data:{
+labels:['Investment','Value'],
+datasets:[{
+label:'Result',
+data:[investment,value],
+backgroundColor:[
+'#3b82f6',
+finalColor
+]
+}]
+},
+options:{
+responsive:true,
+plugins:{
+legend:{display:false}
+}
+}
+});
+
+  }
