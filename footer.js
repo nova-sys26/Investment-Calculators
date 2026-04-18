e0dfead221af695b658ad351c580465e76160f26
Nova-sys <tugrasina@gmail.com>
2026-04-18 09:48:24 +0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -3,5 +3,8 @@ document.write(`
   <a href="privacy.html">Privacy Policy</a>
   <a href="terms.html">Terms of Service</a>
   <a href="contact.html">Contact</a>
+  <p style="margin-top:10px; font-size:12px; color:#94a3b8;">
+© 2026 Nova Calculators
+</p>
 </div>
 `);
