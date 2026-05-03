1228f75327f0a7d6de60d9fc8b4128d11e57482e
Nova-sys <tugrasina@gmail.com>
2026-05-03 23:38:05 +0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@ document.write(`
 <footer class="apple-footer">
 
   <div class="footer-links">
-    <a href="privacy.html">Privacy Policy</a>
+    <a href="privacy.html">Privacy nokta Policy</a>
     <a href="terms.html">Terms of Service</a>
     <a href="contact.html">Contact</a>
   </div>
